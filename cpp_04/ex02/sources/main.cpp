6c62df55f44e4e2d82699c0eae74a17d482ea919
henriksilverbage@gmail.com <henriksilverbage@gmail.com>
2024-02-09 14:23:33 +0100

    cpp04/ex02 fixed

--- cpp_04/ex02/sources/main.cpp
+++ cpp_04/ex02/sources/main.cpp
@@ -19,13 +19,13 @@ int	main (void)
 
 		for (int i = 0; i < 5; i++)
 			Gang[i] = new Dog();
-		for (int i = 5; i < 11; i++)
+		for (int i = 5; i < 10; i++)
 			Gang[i] = new Cat();
 
-		for (int i = 0; i < 11; i++)
+		for (int i = 0; i < 10; i++)
 			Gang[i]->makeSound();
 
-		for (int i = 0; i < 11; i++)
+		for (int i = 0; i < 10; i++)
 			delete	Gang[i];
 	}
 	// {
@@ -42,13 +42,3 @@ int	main (void)
 	// }
 	return (0);
 }
-
-/*
-Dans votre fonction main, créez et remplissez un tableau d’objets Animal dont la
-moitié est composée d’objets Dog et l’autre moitié d’objets Cat. À la fin de l’exécution
-du programme, parcourez ce tableau afin de delete chaque Animal. Vous devez delete
-directement les chiens et les chats en tant qu’Animal. Les destructeurs correspondants
-doivent être appelés dans le bon ordre.
-N’oubliez pas de vérifier que vous n’avez pas de fuites de mémoire.
-La copie d’un objet Dog ou d’un objet Cat ne doit pas être superficielle. Par conséquent, vous devez vous assurer que vos copies sont bien des copies profondes.
-*/
