e37783ba0ac5ea379b7e23977ccdda502a648168
Henrik <henriksilverbage@gmail.com>
2024-03-12 15:58:25 +0100

    all cpp module are done

--- cpp_09/ex02/sources/main.cpp
+++ cpp_09/ex02/sources/main.cpp
@@ -11,49 +11,49 @@ int	main(int argc, char** argv)
 	}
 	PmergeMe	PmergeMe;
 
-	try
+	// try
+	// {
+	// 	std::cout << "SORTING WITH TEMPLATES\n" << std::endl;
+
+	// 	std::vector<unsigned int> 	vec;
+	// 	std::deque<unsigned int>	deck;
+
+	// 	PmergeMe.parsing_template(argv, vec);
+	// 	PmergeMe.parsing_template(argv, deck);
+
+
+	// 	std::cout << "Before:\t";
+	// 	for (size_t i = 0; i < vec.size(); i++)
+	// 		std::cout << vec[i] << " ";
+	// 	std::cout << std::endl;
+
+	// 	clock_t startTime = clock();
+	// 	PmergeMe.sort_template(vec);
+	// 	clock_t endTime = clock();
+	// 	double elapsedTime = (endTime - startTime) / (double)(CLOCKS_PER_SEC);
+
+	// 	std::cout << "After:\t";
+	// 	for (size_t i = 0; i < vec.size(); i++)
+	// 		std::cout << vec[i] << " ";
+	// 	std::cout << std::endl;
+
+	// 	std::cout << std::fixed << std::setprecision(5);
+	// 	std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << elapsedTime << " us" << std::endl;
+
+	// 	startTime = clock();
+	// 	PmergeMe.sort_template(deck);
+	// 	endTime = clock();
+	// 	elapsedTime = (endTime - startTime) / (double)(CLOCKS_PER_SEC);
+
+	// 	std::cout << std::fixed << std::setprecision(5);
+	// 	std::cout << "Time to process a range of " << deck.size() << " elements with std::deque : " << elapsedTime << " us" << std::endl;
+	// }
+	// catch (const std::exception &e)
+	// {
+	// 	std::cout << e.what() << std::endl;
+	// }
 	{
-		std::cout << "SORTING WITH TEMPLATES\n" << std::endl;
-
-		std::vector<unsigned int> 	vec;
-		std::deque<unsigned int>	deck;
-
-		PmergeMe.parsing_template(argv, vec);
-		PmergeMe.parsing_template(argv, deck);
-
-
-		std::cout << "Before:\t";
-		for (size_t i = 0; i < vec.size(); i++)
-			std::cout << vec[i] << " ";
-		std::cout << std::endl;
-
-		clock_t startTime = clock();
-		PmergeMe.sort_template(vec);
-		clock_t endTime = clock();
-		double elapsedTime = (endTime - startTime) / (double)(CLOCKS_PER_SEC);
-
-		std::cout << "After:\t";
-		for (size_t i = 0; i < vec.size(); i++)
-			std::cout << vec[i] << " ";
-		std::cout << std::endl;
-
-		std::cout << std::fixed << std::setprecision(5);
-		std::cout << "Time to process a range of " << vec.size() << " elements with std::vector : " << elapsedTime << " us" << std::endl;
-
-		startTime = clock();
-		PmergeMe.sort_template(deck);
-		endTime = clock();
-		elapsedTime = (endTime - startTime) / (double)(CLOCKS_PER_SEC);
-
-		std::cout << std::fixed << std::setprecision(5);
-		std::cout << "Time to process a range of " << deck.size() << " elements with std::deque : " << elapsedTime << " us" << std::endl;
-	}
-	catch (const std::exception &e)
-	{
-		std::cout << e.what() << std::endl;
-	}
-	{
-		std::cout << "\nSORTING WITH OUT TEMPLATES\n" << std::endl;
+		// std::cout << "\nSORTING WITH OUT TEMPLATES\n" << std::endl;
 
 		if (PmergeMe.parsing(argv) == false)
 			return (-1);
